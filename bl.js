67fa8c5594b014e1b14d72eea49433dd7081d14c
imCCawa <ccawa6688@outlook.com>
2024-10-10 02:12:26 +0800

    Update bl.js

--- bl.js
+++ bl.js
@@ -1,7 +1,7 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-obj.data.expectedMultiple.multiple = 1000000; // 将得分倍率修改为1000000倍
+obj.data.expectedMultiple.multiple = 100; // 将得分倍率修改为1000000倍
 
 body = JSON.stringify(obj);
 $done({body});
