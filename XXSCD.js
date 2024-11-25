55e9e58d409ee4c844f916d6ee191cbf62803652
imCCawa <ccawa6688@outlook.com>
2024-11-25 23:47:07 +0800

    Create XXSCD.js

---
+++ XXSCD.js
@@ -0,0 +1,7 @@
+[rewrite_local]
+# 针对特定网站的响应体重写规则（可以根据需要修改）
+# 这里的规则是示范，不涉及改IP，若需要修改响应体可用此类规则
+# ^http:\/\/psxc\.zaidaxue\.com\/index\.php url script-response-body https://github.com/imCCawa/ccawa/blob/main/XXSCD.JS
+
+[host]
+psxc.zaidaxue.com = 223.153.60.217
