0271fb5ad310bae8cdb6d221555b2619ba78d4e4
imCCawa <ccawa6688@outlook.com>
2024-11-26 00:18:37 +0800

    Update XXSCD.js

--- XXSCD.js
+++ XXSCD.js
@@ -1,8 +1,14 @@
 [rewrite_local]
-^http:\/\/psxc\.zaidaxue\.com\/index\.php url script-response-body https://raw.githubusercontent.com/imCCawa/ccawa/refs/heads/main/XXSCD.js
-  
+^http:\/\/psxc\.zaidaxue\.com\/index\.php url script-request-header https://raw.githubusercontent.com/imCCawa/ccawa/refs/heads/main/XXSCD.js
+
 [mitm]
 hostname = psxc.zaidaxue.com
 
-[hostname]
-psxc.zaidaxue.com=223.153.60.217
+// 以下是XXSCD.js脚本内容
+// 修改请求头，伪造 IP 地址
+var request = $request;
+request.headers['X-Forwarded-For'] = '223.153.60.217';  // 伪造 IP 地址
+request.headers['True-Client-IP'] = '223.153.60.217';    // 伪造 IP 地址
+
+// 返回修改后的请求
+$done({ request: request });
