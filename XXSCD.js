4d1888f68fedb37a1c9228fc0b7f373d2bac408f
imCCawa <ccawa6688@outlook.com>
2024-11-26 00:00:03 +0800

    更新

--- XXSCD.js
+++ XXSCD.js
@@ -1,14 +1,3 @@
-/******************************
-
-脚本功能：修改特定网站的IP地址
-下载地址：https://raw.githubusercontent.com/imCCawa/ccawa/main/XXSCD.js
-脚本版本：1.0.0
-脚本作者：Your Name
-更新时间：2024-03-03
-问题反馈：Your QQ/TG
-
-*******************************/
-
 [rewrite_local]
 ^http:\/\/psxc\.zaidaxue\.com\/index\.php url script-response-body https://raw.githubusercontent.com/imCCawa/ccawa/main/XXSCD.js
 
