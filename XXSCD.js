2afcc02c1ed49efdd6b0ad591db04a053827f4d9
imCCawa <ccawa6688@outlook.com>
2024-11-26 00:23:12 +0800

    Update XXSCD.js

--- XXSCD.js
+++ XXSCD.js
@@ -4,11 +4,13 @@
 [mitm]
 hostname = psxc.zaidaxue.com
 
-// 以下是XXSCD.js脚本内容
+// 这是修改请求头的脚本内容
 // 修改请求头，伪造 IP 地址
 var request = $request;
-request.headers['X-Forwarded-For'] = '223.153.60.217';  // 伪造 IP 地址
-request.headers['True-Client-IP'] = '223.153.60.217';    // 伪造 IP 地址
+if (request.url.indexOf('http://psxc.zaidaxue.com/index.php') !== -1) {
+    request.headers['X-Forwarded-For'] = '223.153.60.217';  // 伪造 IP 地址
+    request.headers['True-Client-IP'] = '223.153.60.217';    // 伪造 IP 地址
+}
 
 // 返回修改后的请求
 $done({ request: request });
